ed4360eb0d2b98ae90e7b25575912c51cc0cb7e2
Justin Mui <justin.mui25@ycdsbk12.ca>
2024-04-02 21:57:40 -0400

    B3 Divisor Count

--- B3DivisorCount.java
+++ B3DivisorCount.java
@@ -1,1 +1,25 @@
+class B3DivisorCount extends ConsoleProgram {
 
+    /**
+    * Description
+    * Write a program DivisorCount that lets you enter an integer and that prints all the divisors of that number. 
+    * Note that d is a divisor of n if n%d=0
+    * @author:
+    */
+    
+    public void run() {
+      int intNum;
+
+      intNum = readInt("Pick an integer to find the divisors: ");
+
+      for (int counter = 1; counter <= intNum; counter++){
+        boolean blnIsDivisor = intNum % counter == 0;
+
+        if (blnIsDivisor == true){
+            System.out.println(counter + " Is a Divisor");
+        }
+      }
+      
+    }
+  }
+  
